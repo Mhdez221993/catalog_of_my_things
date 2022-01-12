c685a2256665bd60c5ea37b59d01218ce570598a
Moises <mhdezcoronado@gmail.com>
2022-01-12 17:04:56 +0100

    Fix linters errors

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -4,7 +4,7 @@ describe MusicAlbum do
   before(:each) do
     mock = double('intance')
     allow(mock).to receive(:items) { [] }
-    @music_album = MusicAlbum.new(mock, mock, mock, mock, '1/12/2010',  true)
+    @music_album = MusicAlbum.new(mock, mock, mock, mock, '1/12/2010', true)
   end
 
   it 'Should initialize the Music class' do
