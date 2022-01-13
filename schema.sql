25c13d5915113535807e30b8cff02911a76a8f8d
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-13 09:24:40 +0100

    Add schema file

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Author (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(120),
+  last_name VARCHAR(120),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE source (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(120),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE label (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(120),
+  color VARCHAR(120),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE book (
+  id INT GENERATED ALWAYS AS IDENTITY ,
+  name VARCHAR(120),
+  publisher VARCHAR(120),
+  cover_state VARCHAR(120),
+  PRIMARY KEY (id),
+  CONSTRAINT id
+    FOREIGN KEY (id)
+      REFERENCES item(id)
+);
+
