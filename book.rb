ac8552cd40afce261de96f6907bff7b7382e448a
Moises <mhdezcoronado@gmail.com>
2022-01-13 08:52:39 +0100

    Create Book class & required methods

---
+++ book.rb
@@ -0,0 +1,37 @@
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :id
+  attr_reader :publisher, :cover_state
+
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(genre, author, source, label, publish_date, publisher, cover_state)
+    super(genre, author, source, label, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+    @publish_date = publish_date
+  end
+  # rubocop:enable Metrics/ParameterLists
+
+  def can_be_archived?
+    super or (cover_state.include? 'bad')
+  end
+
+  def to_s
+    "[Book]: #{super}"
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => id,
+      'genre' => @genre.id,
+      'author' => @author.id,
+      'source' => @source.id,
+      'label' => @label.id,
+      'publish_date' => @publish_date,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state
+    }.to_json(*args)
+  end
+end
