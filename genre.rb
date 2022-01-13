e415e18a84bc1799a267a8115a92d1c2384cf264
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-13 08:54:14 +0100

    Add methods and save to json

--- genre.rb
+++ genre.rb
@@ -8,5 +8,23 @@ class Genre
     @items = []
   end
 
-  
+  def add_item(item)
+    @items << item
+  end
+
+  def to_s
+    "Genre: #{@name}"
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'genre_id' => @id,
+      'name' => @name
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['name'], object['genre_id'])
+  end
 end
