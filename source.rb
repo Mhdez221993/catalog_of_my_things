3deb54667141b167c72f60401c73278681876fe9
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-13 09:28:13 +0100

    Add different methods and save to json

--- source.rb
+++ source.rb
@@ -8,5 +8,23 @@ class Source
     @items = []
   end
 
-  
+  def add_item(item)
+    @items << item
+  end
+
+  def to_s
+    "[Source]: #{@name}"
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'source_id' => @id,
+      'name' => @name
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['name'], object['source_id'])
+  end
 end
