bab192334a3a14d5d26a1a58a1dae7c24cd880e8
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-01-13 09:27:45 +0100

    Add inialize method fr parametes

--- source.rb
+++ source.rb
@@ -2,4 +2,11 @@ class Source
   attr_accessor :id
   attr_reader :name, :items
 
+  def initialize(name = 'Unknown', id = Random.rand(1...1000))
+    @id = id
+    @name = name
+    @items = []
+  end
+
   
+end
